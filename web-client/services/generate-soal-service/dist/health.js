110ec79c2ff84ed44fbdd84f1eac3c2dca591776
hafiizherdian <hafiizh.herdian@gmail.com>
2025-06-28 19:12:41 +0700

    alot

--- web-client/services/generate-soal-service/dist/health.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = health;
-function health(app) {
-    app.get('/health', (req, res) => {
-        res.status(200).send('OK');
-    });
-}
-//# sourceMappingURL=health.js.map
